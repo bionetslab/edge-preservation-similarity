6b9d80606f30b3b914086f47427da97876172b66
jkiederle <jana.kiederle@fau.de>
2022-04-22 16:19:06 +0200

    fixing bug with setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-setup(name='edge_preservation_similarity',
+setup(
+    name='edge_preservation_similarity',
     version='0.1',
     author='Jana Kiederle',
     author_email='jana.kiederle@fau.de',
     packages=find_packages(),
-    install_requires=['numpy', 'time', 'networkx', 'gurobipy'])
+    install_requires=['numpy', 'networkx', 'gurobipy']
+    )
